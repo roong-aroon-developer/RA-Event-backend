a12d5a89c0b6b9671bc8f3d87bb8ada712b98493
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-09-29 15:34:51 +0700

    :hammer: add mongoose model

--- server.ts
+++ server.ts
@@ -2,12 +2,17 @@ import { IUser, UserSchema } from './types';
 import express = require('express');
 import mongoose = require("mongoose");
 
+const url = "mongodb://localhost:27017/node-api"
+
 const app: express.Application = express();
 app.use(express.json);
 
-mongoose.connect('mongodb://localhost:27017/node-api');
+mongoose.connect(url, { useNewUrlParser: true });
 
 const User = mongoose.model<IUser>('Cat', UserSchema);
+const result = new User({ name: "javascritp"});
+
+result.save().then(()=> console.log("meow"))
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).json({
