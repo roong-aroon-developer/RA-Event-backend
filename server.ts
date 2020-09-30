d0739cf1238eed7729293f711e4f82d20268c414
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-09-30 10:42:13 +0700

    :hammer: add err handler

--- server.ts
+++ server.ts
@@ -1,37 +1,55 @@
-import { EventModel } from './models/event';
-import express = require('express');
+import { EventModel } from "./models/event";
+import express = require("express");
 import mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/node-api"
+const url = "mongodb://localhost:27017/node-api";
 const app: express.Application = express();
 
-const db = mongoose.connection
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> {
-    console.log('connected to Mongo!')
-}).catch((err)=> {
+const db = mongoose.connection;
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected to Mongo!");
+  })
+  .catch((err) => {
     console.log(err.message);
-    process.exit(-1)
-})
-app.use(express.json())
+    process.exit(-1);
+  });
+app.use(express.json());
 
-app.post('/events', (req: express.Request, res: express.Response) => {
-    const payload = req.body;
-    const event = new EventModel(payload);
-    event.save().then(()=> 
-    res.status(201).end()
-    ).catch(err=> {
-        console.log(err.message)
-    })
-})
+app.post("/events", (req: express.Request, res: express.Response) => {
+  const payload = req.body;
+  const event = new EventModel(payload);
+  event
+    .save()
+    .then(() => res.status(201).end())
+    .catch((err) => {
+      res.status(400).json(err);
+      console.log(err.message);
+    });
+});
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    const events = EventModel.find({})
-    events.then((data)=> res.status(200).json(data)
-    ).catch(err=> {
-        console.log(err.message);
-    })
+app.get("/", (req: express.Request, res: express.Response) => {
+  const events = EventModel.find({});
+  events
+    .then((data) => res.status(200).json(data))
+    .catch((err) => {
+      res.status(404).json(err);
+      console.log(err.message);
+    });
+});
+
+app.get("event/:name", (req: express.Request, res: express.Response) => {
+  const { name }: any = req.params;
+  const result = EventModel.findOne(name);
+  result
+    .then((data) => res.status(200).json(data))
+    .catch((err) => {
+      res.status(404).json(err);
+      console.log(err.message);
+    });
 });
 
 app.listen(9000, () => {
-    console.log("Server is running on 9000");
+  console.log("Server is running on 9000");
 });
