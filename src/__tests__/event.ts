3c8ca073c525601c4d4eb48b263040fb24c53671
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-10-06 15:34:46 +0700

    :white_check_mark: add GET /events test

--- src/__tests__/event.ts
+++ src/__tests__/event.ts
@@ -2,7 +2,7 @@ import app from '../server'
 import mongoose = require('mongoose')
 import request from "supertest";
 
-describe("GET / - testing 200 OK", () => {
+describe("GET /events - testing 200 OK", () => {
     it("Hello API Request", async () => {
       const result: any = await request(app).get("/events");
       expect(result.statusCode).toEqual(200)
